9582791833ad4437b08070a2513a6f8974e1e6ac
Adriel Alejandro Bergantino <adriel.alejandro1@gmail.com>
2024-11-18 13:41:23 -0300

    Commit push

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args){
-        Menu menu= new Menu();
-        System.out.println("Bienvenido al menu DEF, seleccione una opción:");
-        menu.ejecutarOpcion(2);
-        MaquinaMenor A= new MaquinaMenor("Prueba",10);
-        Producto Ab = new ProductoGeneral("Porton",120,120,"Madera");
-        System.out.println(A.puedeFabricar(Ab));
-        System.out.println(A.UsarMaquina(Ab));
-        System.out.println(A.enUso());
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-        menu.ejecutarOpcion(1);
-    }
-}
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        Scanner scanner = new Scanner(System.in);
+        menu.mostrar();
+        int opcion;
+        try {
+            opcion = scanner.nextInt();
+            menu.ejecutarOpcion(opcion);
+        } catch (InputMismatchException exception) {
+            while (true) {
+                try {
+                    opcion = scanner.nextInt();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Opción incorrecta, ingrese una opción numérica: ");
+                    scanner.nextLine();
+                }
+            }
+
+
+
+                    MaquinaMenor A = new MaquinaMenor("Prueba", 10);
+                    Producto Ab = new ProductoGeneral("Porton", 120, 120, "Madera",30);
+                    System.out.println(A.puedeFabricar(Ab));
+                    System.out.println(A.UsarMaquina(Ab));
+                    System.out.println(A.enUso());
+                }
+            }
+        }
+
+
