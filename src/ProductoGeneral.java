3cecaa1e75bdfee01432e3f0e81702dbe9e0e149
Adriel Alejandro Bergantino <adriel.alejandro1@gmail.com>
2024-11-18 13:12:54 -0300

    Implements Pesable
    Ahora la clase ProductoGeneral implementa la interfaz pesable

--- src/ProductoGeneral.java
+++ src/ProductoGeneral.java
@@ -1,11 +1,28 @@
-public class ProductoGeneral extends Producto {
+public class ProductoGeneral extends Producto implements Pesable {
 
-    public ProductoGeneral(String nombre, double alto, double ancho, String material) {
+    private double precioPorKilo;
+
+    public ProductoGeneral(String nombre, double alto, double ancho, String material, double precioPorKilo) {
         super(nombre, alto, ancho, material);
+        this.precioPorKilo = precioPorKilo;
     }
 
     @Override
     public boolean verificarProduccion(Maquina maquina) {
         return maquina.puedeFabricar(this);
     }
+
+    @Override
+    public double calcularPrecioPorPeso(double peso) {
+        try {
+            if (peso <= 0) {
+                throw new IllegalArgumentException("El peso debe ser mayor a 0");
+            }
+            return peso * precioPorKilo;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            return 0;
+        }
+    }
 }
+
