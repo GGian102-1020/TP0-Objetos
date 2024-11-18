d85e32b55b879cd386e4b40bd8c4e457ecf29e97
Nicolás D <96535022+Nico2430@users.noreply.github.com>
2024-11-18 13:21:43 -0300

    Agrego excepción en Main, genero algunos casos del "Menu"

--- src/Menu.java
+++ src/Menu.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Menu implements MenuA{
+    private int intentos = 0;
     private int password = 2430;
     private int PassIngresada;
     Scanner scanner = new Scanner(System.in);
@@ -20,20 +21,44 @@ public class Menu implements MenuA{
                 System.out.println("1. Crear numero de cliente");
                 System.out.println("2. Ingresar numero de cliente");
                 System.out.println("3. Volver atrás");
+                opcion=scanner.nextInt();
+                switch(opcion){
+                    case 1:
+                        System.out.println("Ingresar el nombre de cliente: ");
+                        String NombreCliente= scanner.nextLine();
+
+
+
+                    case 2:
+
+
+
+                    case 3:
+                        break;
+                }
 
             case 2:
                 System.out.println("Ingrese la contraseña: ");
                 int PassIngresada= scanner.nextInt();
+                while (intentos<2)
                 if (PassIngresada==password){
+                    intentos+=1;
                     System.out.println("1. Ver pedidos por cliente");
                     System.out.println("2. Ver cola por máquinas");
-                    opcion= scanner.nextInt();
-                    switch (opcion) {
+                    int opcionA= scanner.nextInt();
+                    switch (opcionA) {
                         case 1:
-                            System.out.println("Bien, funciona!");
+                            System.out.println("Ingrese el número de cliente: ");
+                            int nCliente= scanner.nextInt();
+
 
 
                     }
+                }
+                else{
+                    intentos+=1;
+                    System.out.println("Contraseña incorrecta, le quedan " + (3-intentos) + " intentos más:");
+                    PassIngresada= scanner.nextInt();
                 }
 
                 break;
