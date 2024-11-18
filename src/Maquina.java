80ac3635653ebf7d8e8e5c511cb12c9b9815c477
lucaslarocca <164672197+lucaslarocca@users.noreply.github.com>
2024-11-18 19:00:37 -0300

    Update Maquina.java

--- src/Maquina.java
+++ src/Maquina.java
@@ -3,7 +3,10 @@ public abstract class Maquina {
     private String nombre;
     private double costoOperacion;
 
-    public abstract void crearMaquina (String nombre, double costoOperacion);
+    public Maquina (String nombre, double costoOperacion){
+        this.nombre = nombre;
+        this.costoOperacion = costoOperacion;
+    }
 
     public abstract boolean puedeFabricar(Producto producto);
 
